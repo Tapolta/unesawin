f513703b7941c13940ea06ee75a8aff8970b2d3f
tapolta <gustavoltawork@gmail.com>
2025-11-23 22:31:04 +0700

    benerin rekaman dikit

--- src/routes/Record.svelte
+++ src/routes/Record.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import { AudioLines, Mic } from "@lucide/svelte";
-  import { push } from "svelte-spa-router";
+  import { AudioLines, LoaderCircle, Mic } from "@lucide/svelte";
+  import { push } from 'svelte-spa-router';
 
   let isRecord = $state(false);
   let afterRecord = $state(false);
@@ -11,9 +11,11 @@
 
   const handlePointerUp = () => {
     onRecord(false);
-
-    // push('/record-check/1');
     afterRecord = true;
+
+    setTimeout(() => {
+      push("/record-check/coba");
+    }, 6000);
   }
 
   const handlePointerDown = () => {
@@ -22,14 +24,11 @@
 </script>
 
 <div 
-  class="relative flex justify-center items-center flex-col max-w-xl bg-three min-h-screen w-[100%] mx-auto"
+  class="relative flex justify-center items-center flex-col max-w-xl bg-three h-screen w-[100%] mx-auto"
 >
+{#if !afterRecord}
   <h4 class="absolute -translate-y-48 text-two text-3xl font-bold">
-    {#if isRecord}
-    Sedang direkam
-    {:else}
     Cobalah Batuk
-    {/if}
   </h4>
   
   <div class="rounded-full p-10 bg-one">
@@ -47,4 +46,26 @@
       {/if}
     </button>
   </div>
+
+  <p class="absolute translate-y-48 text-xl font-bold italic">
+  {#if isRecord} 
+    Mendengarkan...
+  {:else}
+    Tahan untuk merekam
+  {/if}
+  </p>
+{:else}
+  <h6 class="absolute -translate-y-32 italic font-bold text-xl">
+    Menganalisis Batuk Anda...
+  </h6>
+
+  <div>
+    <LoaderCircle class="w-42 h-42 animate-spin text-gray-400" />
+  </div>
+
+  <p class="text-xs text-center w-xs px-4 absolute translate-y-32">
+    Harap tunggu sejenak. Sistem AI kami sedang memproses pola dan frekuensi batuk Anda.
+  </p>
+  <p></p>
+  {/if}
 </div>
